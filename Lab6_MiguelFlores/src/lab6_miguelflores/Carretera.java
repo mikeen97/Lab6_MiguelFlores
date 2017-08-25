15c4dd616a7fdff2883d3e1469cbc1fcabed7731
mikeen97 <mike97@hotmail.es>
2017-08-25 15:05:12 -0600

    creacion del menu para crear carreteras

--- Lab6_MiguelFlores/src/lab6_miguelflores/Carretera.java
+++ Lab6_MiguelFlores/src/lab6_miguelflores/Carretera.java
@@ -11,8 +11,56 @@ package lab6_miguelflores;
  */
 public class Carretera {
 
+    private int numero;
+    private double distancia;
+    private Lugares inicio;
+    private Lugares fin;
 
-    
-    
+    public Carretera() {
+    }
+
+    public Carretera(int numero, double distancia, Lugares inicio, Lugares fin) {
+        this.numero = numero;
+        this.distancia = distancia;
+        this.inicio = inicio;
+        this.fin = fin;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+
+    public Lugares getInicio() {
+        return inicio;
+    }
+
+    public void setInicio(Lugares inicio) {
+        this.inicio = inicio;
+    }
+
+    public Lugares getFin() {
+        return fin;
+    }
+
+    public void setFin(Lugares fin) {
+        this.fin = fin;
+    }
+
+    @Override
+    public String toString() {
+        return "Carretera Nº: "+numero + "";
+    }
 
 }
--- Lab6_MiguelFlores/src/lab6_miguelflores/Lugares.java
+++ Lab6_MiguelFlores/src/lab6_miguelflores/Lugares.java
@@ -12,66 +12,65 @@ import java.util.ArrayList;
  * @author Miguel Flores
  */
 public class Lugares {
-    
     private String nombre;
     private String direccion;
     private int Seguridad;
     private ArrayList<Carretera> ListEntradas = new ArrayList();
     private Carretera Salida;
-    
+
     public Lugares() {
     }
-    
+
     public Lugares(String nombre, String direccion, int Seguridad, Carretera Salida) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.Seguridad = Seguridad;
         this.Salida = Salida;
     }
-    
+
     public String getNombre() {
         return nombre;
     }
-    
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    
+
     public String getDireccion() {
         return direccion;
     }
-    
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
-    
+
     public int getSeguridad() {
         return Seguridad;
     }
-    
+
     public void setSeguridad(int Seguridad) {
         this.Seguridad = Seguridad;
     }
-    
+
     public ArrayList<Carretera> getListEntradas() {
         return ListEntradas;
     }
-    
+
     public void setListEntradas(Carretera x) {
         ListEntradas.add(x);
     }
-    
+
     public Carretera getSalida() {
         return Salida;
     }
-    
+
     public void setSalida(Carretera Salida) {
         this.Salida = Salida;
     }
-    
+
     @Override
     public String toString() {
         return nombre;
     }
-    
+
 }
